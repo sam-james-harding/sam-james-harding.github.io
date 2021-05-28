8cbc595c4ef7592ce4c2d34a86e30a6cb702c54b
Sam Harding <48120625+sam-james-harding@users.noreply.github.com>
2021-05-28 13:47:55 +1000

    Added repos page

---
+++ javascript/app.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
---
+++ javascript/repos.js
@@ -0,0 +1,42 @@
+//api call
+const Http = new XMLHttpRequest()
+const url = 'https://api.github.com/users/sam-james-harding/repos'
+Http.open("GET", url)
+Http.send()
+
+//api recieve
+Http.onreadystatechange=(e)=>{
+    var response = Http.responseText
+    var reposData = JSON.parse(response)
+    formatReposList(reposData)
+}
+
+function formatReposList(reposData) {
+    //site elements
+    const repoDisplay = document.getElementById("repoDisplay")
+
+    //creating html for div insert
+    reposHTML = `<h1>My GitHub Repositories</h1>`
+
+    for (i=0;i<reposData.length;i++) {
+        var name  = reposData[i].name
+        var description = reposData[i].description
+        var link = reposData[i].html_url
+
+        if (name=="sam-james-harding.github.io") {
+            continue;
+        }
+
+        reposHTML += `<a href="${link}"><h3>${name}</h3></a>`
+
+        if (!description) {
+            reposHTML += `<p><i>No description provided</i></p>`
+        }
+        else {
+            reposHTML += `<p>${description}</p>`
+        }
+
+    }
+
+    repoDisplay.innerHTML = reposHTML
+}
