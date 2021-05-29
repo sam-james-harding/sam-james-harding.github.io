69f5cd4bd818c27a8ab4db39274ac94ee856d4b3
Sam Harding <48120625+sam-james-harding@users.noreply.github.com>
2021-05-29 17:51:27 +1000

    Menu bar links

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,7 +1,7 @@
 var navbar = document.createElement("div")
 
 navbar.innerHTML = `
-    <a>Home</a>
+    <a href="home.html">Home</a>
     <a href="repos.html">GitHub Repositories</a>
     <a>3D Printing</a>
     `
