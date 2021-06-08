3dfc164f4928e3e4e3aeeb6b0aa9429f369559b7
Sam Harding <48120625+sam-james-harding@users.noreply.github.com>
2021-06-08 12:17:24 +1000

    Many many changes

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -13,22 +13,20 @@ const pages = [
     ["Home", "home.html"],
     ["GitHub Repositories", "repos.html"],
     ["3D Printing", "3dprinting.html"],
-    ["Electronics", "electronics.html"],
-    ["Games", "games.html"]
+    ["Electronics", "electronics.html"]
 ]
 
-navbarHTML = ``
+
+var navbarHTML = ``
 for (i=0; i<pages.length; i++) {
     var name = pages[i][0]
     var link = pages[i][1]
 
     if (link == title) {
-        navbarHTML += `<a href="${link}" style="background-color:rgb(87, 167, 115); color: white;">${name}</a>`
+        navbarHTML += `<a href="${link}" id="selected">${name}</a>`
     } else {
         navbarHTML += `<a href="${link}">${name}</a>`
     }
-
-    
 }
 
 navbar.innerHTML = navbarHTML
